01e78816a40426619989adb9b115dcc8957c3586
nanff <13641286615@163.com>
2022-07-23 12:20:07 +0800

    1

---
+++ src/test/java/org/example/TestDemo.java
@@ -0,0 +1,23 @@
+package org.example;
+
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class TestDemo {
+    @BeforeClass
+    public void before(){
+        System.out.println("我是BeforeClass");
+    }
+    @Test
+    public void test1(){
+        System.out.println("打开百度");
+
+    }
+    @AfterClass
+    public void after(){
+        System.out.println("我是AfterClass");
+
+    }
+
+}
